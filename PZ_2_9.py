d5c6fd1540ceaf0d8e18de0f550f1d39b679fa05
nikitakorennoi <144542557+nikitakorennoi@users.noreply.github.com>
2023-09-09 18:53:17 +0300

    Add files via upload

---
+++ PZ_2_9.py
@@ -0,0 +1,4 @@
+otrezokA = int(input("Vvedite dlinu otrezka A: "))
+otrezokB = int(input("Vvedite dlinu otrezka B: "))
+kolichestvo = otrezokA // otrezokB
+print(f"Kolichestvo otrezkov B v otrezke A: {kolichestvo}")
